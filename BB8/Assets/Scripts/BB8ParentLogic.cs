4421ef54e7d9e62fb261289ecbfe587afb40a9a0
jlisooey <jlisooey@gmail.com>
2020-07-19 19:15:28 -0400

    Created copy of prefab "BB-9", and copied the movement script.  Controller moves around and the head stay atop.  Only usable with generic spheres, imported models have an issue with pivot being not aligned to center.

---
+++ BB8/Assets/Scripts/BB8ParentLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BB8ParentLogic : MonoBehaviour
+{
+    public Transform m_BodyTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = m_BodyTransform.position;
+    }
+}
